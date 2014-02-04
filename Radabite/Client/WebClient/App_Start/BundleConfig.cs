7a58c169caf70eafeb5c8767788e7da36f324fc9
clottman <clottman@live.com>
2014-02-04 16:21:19 -0600

    added path to bundle names

--- Radabite/Client/WebClient/App_Start/BundleConfig.cs
+++ Radabite/Client/WebClient/App_Start/BundleConfig.cs
@@ -9,46 +9,46 @@ namespace Radabite
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Client/WebClient/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+                        "~/Client/WebClient/Scripts/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+                        "~/Client/WebClient/Scripts/jquery.unobtrusive*",
+                        "~/Client/WebClient/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Client/WebClient/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Client/WebClient/Content/css").Include("~/Content/site.css"));
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            bundles.Add(new StyleBundle("~/Client/WebClient/Content/themes/base/css").Include(
+                        "~/Client/WebClient/Content/themes/base/jquery.ui.core.css",
+                        "~/Client/WebClient/Content/themes/base/jquery.ui.resizable.css",
+                        "~/Client/WebClient/Content/themes/base/jquery.ui.selectable.css",
+                        "~/Client/WebClient/Content/themes/base/jquery.ui.accordion.css",
+                        "~/Client/WebClient/Content/themes/base/jquery.ui.autocomplete.css",
+                        "~/Client/WebClient/Content/themes/base/jquery.ui.button.css",
+                        "~/Client/WebClient/Content/themes/base/jquery.ui.dialog.css",
+                        "~/Client/WebClient/Content/themes/base/jquery.ui.slider.css",
+                        "~/Client/WebClient/Content/themes/base/jquery.ui.tabs.css",
+                        "~/Client/WebClient/Content/themes/base/jquery.ui.datepicker.css",
+                        "~/Client/WebClient/Content/themes/base/jquery.ui.progressbar.css",
+                        "~/Client/WebClient/Content/themes/base/jquery.ui.theme.css"));
 
             #region Foundation Bundles
                 bundles.Add(new StyleBundle("~/Content/foundation/css").Include(
-                    "~/Content/foundation/foundation.css",
-                    "~/Content/foundation/foundation.mvc.css",
-                    "~/Content/foundation/app.css"));
+                    "~/Client/WebClient/Content/foundation/foundation.css",
+                    "~/Client/WebClient/Content/foundation/foundation.mvc.css",
+                    "~/Client/WebClient/Content/foundation/app.css"));
 
                 bundles.Add(new ScriptBundle("~/bundles/foundation").Include(
-                    "~/Scripts/foundation/foundation.js",
-                    "~/Scripts/foundation/foundation.*",
-                    "~/Scripts/foundation/app.js"));
+                    "~/Client/WebClient/Scripts/foundation/foundation.js",
+                    "~/Client/WebClient/Scripts/foundation/foundation.*",
+                    "~/Client/WebClient/Scripts/foundation/app.js"));
             #endregion
         }
     }
