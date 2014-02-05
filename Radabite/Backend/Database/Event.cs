652146a7d4e929c263b5d897425c69f6d065e0eb
clottman <clottman@live.com>
2014-02-04 20:53:36 -0600

    changed some attribute names

--- Radabite/Backend/Database/Event.cs
+++ Radabite/Backend/Database/Event.cs
@@ -8,7 +8,7 @@ namespace Radabite.Backend.Database
 {
     public class Event
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
 
         public string Title { get; set; }
 
--- Radabite/Backend/Database/User.cs
+++ Radabite/Backend/Database/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +8,15 @@ namespace Radabite.Backend.Database
 {
     public class User
     {
-        public int Id { get; set; }
+        [Key]
+        public long Id { get; set; }
         public string DisplayName { get; set; }
         public string TwitterHandle { get; set; }
         public string FacebookName { get; set; }
+        public long TwitterUserId { get; set; }
+        public long FacebookUserId { get; set; }
         public string Email { get; set; }
-        public string LinkToPhoto { get; set; }
+        public string PhotoLink { get; set; }
         public string SelfDescription { get; set; }
         public int Age { get; set; }
 
