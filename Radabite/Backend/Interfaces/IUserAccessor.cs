216a50a3dbf34fa7ccb2be7e811dcc62c3e8ba9c
clottman <clottman@live.com>
2014-02-04 21:15:10 -0600

    backend call chain for user done

---
+++ Radabite/Backend/Interfaces/IUserAccessor.cs
@@ -0,0 +1,15 @@
+﻿using Radabite.Backend.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radabite.Backend.Interfaces
+{
+    interface IUserAccessor
+    {
+        User GetById(long id);
+        void Save(User user);
+    }
+}
