d54ac357e181df56fa4738c19323315123bfc7c1
clottman <clottman@live.com>
2014-02-06 16:59:19 -0600

    made some attempts but no clue whats going on

---
+++ Radabite/Backend/Managers/TwitterManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using TweetSharp;
+
+namespace Radabite.Backend.Managers
+{
+    public class TwitterManager
+    {   
+
+
+    }
+}
