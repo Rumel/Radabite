345a6c0274a0ec42e56d18a1e4ec066e220d4d84
NathanDeMaria <naterstd@gmail.com>
2014-02-11 21:59:11 -0600

    Inital wireframe for event page

--- Radabite/Client/WebClient/Controllers/EventController.cs
+++ Radabite/Client/WebClient/Controllers/EventController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Radabite.Backend.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,13 +11,29 @@ namespace Radabite.Client.WebClient.Controllers
     {
         //
         // GET: /Event/
-
         public ActionResult Index(long eventId)
         {
 			ViewBag.Message = "Event " + eventId.ToString();
 			ViewBag.eventId = eventId;
 
-            return View();
+			Event dummy = new Event()
+			{
+				Id = 1,
+				Title = "G-Ma's 9th birthday",
+				StartTime = new DateTime(2014, 1, 1, 1, 1, 1),
+				EndTime = new DateTime(2014, 1, 1, 1, 1, 2),
+				IsPrivate = true,
+				Description = "Happy Birthday Grandma",
+				Location = new Location()
+				{
+					LocationId = 1,
+					LocationName = "My house",
+					Latitude = 1.01,
+					Longitude = 1.01
+				}
+			};
+
+            return View(dummy);
         }
 
 		public ActionResult CreateEvent(long userId)
