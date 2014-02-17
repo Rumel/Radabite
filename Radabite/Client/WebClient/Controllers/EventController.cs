157434e61286dd391a642e8b6991203843f94336
Sawyer Jager <sawyer.jgr@gmail.com>
2014-02-16 21:43:11 -0600

    Added Post function to Event Controller

--- Radabite/Client/WebClient/Controllers/EventController.cs
+++ Radabite/Client/WebClient/Controllers/EventController.cs
@@ -1,4 +1,5 @@
-﻿using Radabite.Backend.Database;
+﻿using Radabite.Backend.Accessors;
+using Radabite.Backend.Database;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,8 @@ namespace Radabite.Client.WebClient.Controllers
 {
     public class EventController : Controller
     {
+        EventAccessor eventAccessor = new EventAccessor();
+
         //
         // GET: /Event/
         public ActionResult Index(long eventId, long userId)
@@ -17,24 +20,29 @@ namespace Radabite.Client.WebClient.Controllers
 			ViewBag.eventId = eventId;
 			ViewBag.userId = userId;
 
-			Event dummy = new Event()
-			{
-				Id = 1,
-				Title = "G-Ma's 9th birthday",
-				StartTime = new DateTime(2014, 1, 1, 1, 1, 1),
-				EndTime = new DateTime(2014, 1, 1, 1, 1, 2),
-				IsPrivate = true,
-				Description = "Happy Birthday Grandma",
-				Location = new Location()
-				{
-					LocationId = 1,
-					LocationName = "My house",
-					Latitude = 1.01,
-					Longitude = 1.01
-				}
-			};
+            Event eventRequest = (Event) eventAccessor.GetAll().Where(userEvent => userEvent.Id == eventId);
 
-            return View(dummy);
+            if (eventRequest == null)
+            {
+                eventRequest = new Event()
+                {
+                    Id = 1,
+                    Title = "G-Ma's 9th birthday",
+                    StartTime = new DateTime(2014, 1, 1, 1, 1, 1),
+                    EndTime = new DateTime(2014, 1, 1, 1, 1, 2),
+                    IsPrivate = true,
+                    Description = "Happy Birthday Grandma",
+                    Location = new Location()
+                    {
+                        LocationId = 1,
+                        LocationName = "My house",
+                        Latitude = 1.01,
+                        Longitude = 1.01
+                    }
+                };
+            }
+
+            return View(eventRequest);
         }
 
 		public ActionResult CreateEvent(long userId)
@@ -53,5 +61,27 @@ namespace Radabite.Client.WebClient.Controllers
 			return View();
 		}
 
+        [HttpPost]
+        public RedirectToRouteResult Create(string title, long startTime, long endTime/*, Location location*/)
+        {
+            Event newEvent = new Event();
+            newEvent.Id = 321;
+            newEvent.StartTime = new DateTime(startTime);
+            newEvent.EndTime = new DateTime(endTime);
+            newEvent.Location = new Location()
+            {
+                LocationId = 1,
+                LocationName = "My house",
+                Latitude = 1.01,
+                Longitude = 1.01
+            };
+            newEvent.IsPrivate = false;
+            newEvent.Title = title;
+
+
+            //eventAccessor.Save(newEvent);
+
+            return RedirectToAction("Index", new { userId = 123, eventId = 321 });
+        }
     }
 }
