6fb50f51e786c1c70de7eb8d2c1c03783ff65984
NathanDeMaria <naterstd@gmail.com>
2014-02-17 23:24:37 -0600

    Turned friends table into a partial

--- Radabite/Client/WebClient/Controllers/UserProfileController.cs
+++ Radabite/Client/WebClient/Controllers/UserProfileController.cs
@@ -4,14 +4,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ninject;
+using RadabiteServiceManager;
+using Radabite.Backend.Interfaces;
 
 namespace Radabite.Client.WebClient.Controllers
 {
-    public class UserProfileController : Controller
-    {
+	public class UserProfileController : Controller
+	{
 
-        public ActionResult Index(long userId)
+		public ActionResult Index(long userId)
         {
+            ViewBag.Message = userId.ToString() + "'s profile page.";
+            ViewBag.userId = userId;
+
+
+			IList<User> friends = new List<User>();
+
+			//needs database to work
+			//friends = ServiceManager.Kernel.Get<IUserManager>().GetById(userId).Friends;
+
+			//dummy friends
+			for (int i = 0; i < 5; i++)
+			{
+				friends.Add(new User()
+				{
+					Id = i,
+					DisplayName = "Person " + i
+				});
+			}
+			ViewBag.Friends = friends;
 
             //Get list of events that user is involved in
             List<Event> eventList = new List<Event>();
@@ -26,11 +48,9 @@ namespace Radabite.Client.WebClient.Controllers
                 eventList.ElementAt<Event>(i-90).Description = "We are going to party " + i + "eva";
             }
 
-            ViewBag.Message = userId.ToString() + "'s profile page.";
-            ViewBag.userId = userId;
 
             return View(eventList);
         }
 
-    }
+	}
 }
