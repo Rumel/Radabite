bcf974c3b814f2104ac1ab23d7273bb9948e6f35
NathanDeMaria <naterstd@gmail.com>
2014-02-20 18:10:34 -0600

    Switch UserProfile to use the ViewModel

--- Radabite/Client/WebClient/ViewModels/ViewModel.cs
+++ Radabite/Client/WebClient/ViewModels/ViewModel.cs
@@ -6,6 +6,8 @@ using Radabite.Backend.Database;
 
 namespace Radabite.Client.WebClient.ViewModels
 {
+	//probably shouldn't be named this in case we want to make other ViewModels
+		//but not sure how to name because I don't know exactly how I'm going to use this
 	public class ViewModel
 	{
 		public List<User> Friends { get; set; }
