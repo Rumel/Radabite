57410c8ce6ed7dc49bbc0699d49f6f11bf56dce3
clottman <clottman@live.com>
2014-03-04 22:24:18 -0600

    added some time converter stuff

--- Radabite/Backend/Managers/EventManager.cs
+++ Radabite/Backend/Managers/EventManager.cs
@@ -6,6 +6,7 @@ using Radabite.Backend.Database;
 using Radabite.Backend.Interfaces;
 using Ninject;
 using RadabiteServiceManager;
+using Facebook;
 
 namespace Radabite.Backend.Managers
 {
@@ -26,6 +27,21 @@ namespace Radabite.Backend.Managers
         public Event GetById(long id)
         {
             return ServiceManager.Kernel.Get<IEventAccessor>().GetById(id);
+        }
+
+        public double  ConvertToFacebookTime(DateTime startTime) 
+        {
+            DateTime epoch = DateTimeConvertor.Epoch;
+            int pdtOffset = -25200; // pacific daylight time offeset in seconds
+            DateTimeOffset dtOffset = new DateTimeOffset(startTime, TimeSpan.FromSeconds(pdtOffset));
+            double unixTime = DateTimeConvertor.ToUnixTime(dtOffset);
+            return unixTime;
+        }
+
+        public DateTime ConvertFromUnixTime(double startTime)
+        {
+            DateTime result = DateTimeConvertor.FromUnixTime(startTime);
+            return result;
         }
     }
 }
