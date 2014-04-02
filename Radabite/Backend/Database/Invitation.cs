3f2884097f51a54f8dad647cfa270ce5469156fe
Dalton Dick <daltondick2010@gmail.com>
2014-04-01 20:10:40 -0500

    Remove duplicate app.config in test project

---
+++ Radabite/Backend/Database/Invitation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Radabite.Backend.Database
+{
+    public class Invitation : DataObject
+    {
+        public User Guest { get; set; }
+
+        public ResponseType Response { get; set; }
+    }
+
+    public enum ResponseType
+    {
+        Accepted,
+        Rejeced,
+        WaitingReply
+    }
+}
