0f0e7fcd3a250953c3b7ef89dbf484923a9bbdd8
NathanDeMaria <naterstd@gmail.com>
2014-04-10 18:01:47 -0500

    Added SolverFoundation

---
+++ Radabite/Backend/Helpers/FooSimplex.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.SolverFoundation.Solvers;
+
+namespace Radabite.Backend.Helpers
+{
+	public class FooSimplex
+	{
+		private  SimplexSolver _solver;
+
+		public FooSimplex()
+		{
+			_solver = new SimplexSolver();
+		}
+
+		public void RunLP()
+		{
+			//lol :)
+		}
+	}
+}
