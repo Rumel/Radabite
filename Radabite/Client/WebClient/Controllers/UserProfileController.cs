acdebf35db2f8514ee3e941f32e5170822946d50
clottman <clottman@live.com>
2014-04-20 20:00:00 -0500

    only display profile if a user is logged in

--- Radabite/Client/WebClient/Controllers/UserProfileController.cs
+++ Radabite/Client/WebClient/Controllers/UserProfileController.cs
@@ -20,18 +20,26 @@ namespace Radabite.Client.WebClient.Controllers
 
 		public ActionResult Index(int u)
         {
+            
             var user = ServiceManager.Kernel.Get<IUserManager>().GetById(u);
-            var userModel = new UserModel { User = user };
+            if (user != null)
+            {
+                var userModel = new UserModel { User = user };
 
-			//TODO: Add friends to UserProfile, so there will actually be friends in the db
-			//var friends = ServiceManager.Kernel.Get<IUserManager>().GetById(userId).Friends;
-			//dummy empty list of friends
-			var friends = new List<User>();			
+                //TODO: Add friends to UserProfile, so there will actually be friends in the db
+                //var friends = ServiceManager.Kernel.Get<IUserManager>().GetById(userId).Friends;
+                //dummy empty list of friends
+                var friends = new List<User>();
 
-            //Get list of events that user is involved in
-            userModel.DiscoverEvents = ServiceManager.Kernel.Get<IEventManager>().GetByOwnerId(user.Id);
-            userModel.Friends = friends;
-            return View(userModel);
+                //Get list of events that user is involved in
+                userModel.DiscoverEvents = ServiceManager.Kernel.Get<IEventManager>().GetByOwnerId(user.Id);
+                userModel.Friends = friends;
+                return View(userModel);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
         }
 
         public ActionResult CurrentUser()
