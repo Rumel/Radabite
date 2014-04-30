2715f6e9af6bebfb23a5217842cf3433ae72d44f
Sawyer Jager <sawyer.jgr@gmail.com>
2014-04-29 20:43:44 -0500

    Remove access to pages when not logged in.

--- Radabite/Client/WebClient/Controllers/UserProfileController.cs
+++ Radabite/Client/WebClient/Controllers/UserProfileController.cs
@@ -21,15 +21,11 @@ namespace Radabite.Client.WebClient.Controllers
 
 		public ActionResult Index(int u)
         {
-            
             var user = ServiceManager.Kernel.Get<IUserManager>().GetById(u);
             if (user != null)
             {
                 var userModel = new UserModel { User = user };
 
-                //TODO: Add friends to UserProfile, so there will actually be friends in the db
-                //var friends = ServiceManager.Kernel.Get<IUserManager>().GetById(userId).Friends;
-                //dummy empty list of friends
                 var friends = ServiceManager.Kernel.Get<IUserManager>().GetAll().Where(x => x.Id != user.Id).ToList();
 
                 //Get list of events that user is involved in
