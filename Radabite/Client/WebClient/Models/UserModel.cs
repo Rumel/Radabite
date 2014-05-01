a9e109c44b81e717e29a049682ec70934eb8ced9
Sawyer Jager <sawyer.jgr@gmail.com>
2014-05-01 01:56:06 -0500

    Add a flippin calendar

--- Radabite/Client/WebClient/Models/UserModel.cs
+++ Radabite/Client/WebClient/Models/UserModel.cs
@@ -1,8 +1,10 @@
-﻿using Radabite.Backend.Database;
+﻿using Newtonsoft.Json;
+using Radabite.Backend.Database;
 using Radabite.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace Radabite.Client.WebClient.Models
@@ -15,5 +17,38 @@ namespace Radabite.Client.WebClient.Models
         public List<EventModel> DiscoverEvents { get; set; }
         public List<Event> EventInvitations { get; set; }
         public List<User> Friends { get; set; }
+
+        public string ToCalendarJSon()
+        {
+            var events = new List<JsonEvent>();
+            foreach (var e in DiscoverEvents)
+            {
+                events.Add(new JsonEvent
+                {
+                    date = ((e.StartTime.Ticks - new DateTime(1970, 1, 1, 0, 0, 0, 0).Ticks)/10000).ToString(),
+                    type = "meeting",
+                    title = e.Title,
+                    description = (e.Description == null ? "" : e.Description),
+                    url = "../Event?eventId=" + e.Id
+                });
+            }
+
+            return JsonConvert.SerializeObject(events);
+        }
+
+        internal class JsonEvent
+        {
+            public string date;
+
+            public string type;
+
+            public string title;
+
+            public string description;
+
+            public string url;
+        }
     }
+
+
 }
